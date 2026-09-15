9f5ea0ed33680f13bd47249ef4a510db9ac4afae
lucasgondrumond-maker <lucasgondrumond@gmail.com>
2026-09-15 12:16:38 -0300

    state.py

--- mindos/state.py
+++ mindos/state.py
@@ -1,11 +1,7 @@
-from typing import List, Optional
-from typing_extensions import TypedDict
+from typing import TypedDict, List, Annotated
+import operator
 
-class MindosState(TypedDict):
-    user_goal: str
-    active_agents: List[str]
-    strategic_plan: Optional[str]
-    marketing_strategy: Optional[str]
-    execution_status: str
-    logs: List[str]
-    errors: List[str]
+class AgentState(TypedDict):
+    objective: str
+    response: str
+    messages: List[str]
